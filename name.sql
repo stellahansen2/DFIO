71771ef5148e4972bcd1a672c22adfc64923ff86
Stella Hansen <458537@ac02zc4tzlvdq.genpt.net>
2022-07-27 13:08:03 -0400

    name.sql edited by 0

--- name.sql
+++ name.sql
@@ -1,2 +1,30 @@
-/* comment
-I added a comment*/
+create or replace PROCEDURE TEST_CI_CD AS 
+
+cursor csr_test is
+  select name,TABLECHANGED from CX_TEST_CICD_SCRIPT;
+
+BEGIN
+
+FOR csr_test_v IN csr_test LOOP
+    
+          begin
+            update CX_TEST_CICD_SCRIPT
+              set  name = CONCAT(name, '_changed')
+                 
+            where TABLECHANGED = 'changed';
+              
+          EXCEPTION when OTHERS then
+            raise_application_error(-20010,'Error');
+          end;  
+          
+    end LOOP;
+    
+    commit;
+
+  
+END TEST_CI_CD;
+/
+
+/* Added a comment.
+added another line
+*/
