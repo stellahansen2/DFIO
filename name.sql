96692896796ab3ce8dd3878b4a86f42cdc44cff6
Stella Hansen <458537@ac02zc4tzlvdq.genpt.net>
2022-07-27 13:17:14 -0400

    name.sql edited by 0

--- name.sql
+++ name.sql
@@ -1,4 +1,12 @@
-create or replace PROCEDURE TEST_CI_CD AS 
+--------------------------------------------------------
+--  File created - Wednesday-July-27-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Procedure TEST_CI_CD
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE EDITIONABLE PROCEDURE "GPC"."TEST_CI_CD" AS
 
 cursor csr_test is
   select name,TABLECHANGED from CX_TEST_CICD_SCRIPT;
@@ -6,26 +14,22 @@ cursor csr_test is
 BEGIN
 
 FOR csr_test_v IN csr_test LOOP
-    
+
           begin
             update CX_TEST_CICD_SCRIPT
               set  name = CONCAT(name, '_changed')
-                 
+
             where TABLECHANGED = 'changed';
-              
+
           EXCEPTION when OTHERS then
             raise_application_error(-20010,'Error');
-          end;  
-          
+          end;
+
     end LOOP;
-    
+
     commit;
 
-  
-END TEST_CI_CD;
-/
 
-/* Added a comment.
-added another line
-add another line
-*/
+END TEST_CI_CD;
+
+/
